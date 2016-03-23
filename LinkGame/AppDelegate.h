d85cbf56b5f4996b6129e9e6b1bb5687fea1b653
lizhenZuo <1732096868@qq.com>
2016-03-23 15:50:01 +0800

    Initial Commit

---
+++ LinkGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkGame
+//
+//  Created by zorro on 16/3/23.
+//  Copyright © 2016年 zorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LinkGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinkGame
+//
+//  Created by zorro on 16/3/23.
+//  Copyright © 2016年 zorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
